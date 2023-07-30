7122a8848adcb834dc1d6e76708cdd6479254ee2
Hajime Ozawa <haji.register@gmail.com>
2023-07-31 01:02:54 +0900

    Define class for items

--- scrapy_jd_green/scrapy_jd_green/items.py
+++ scrapy_jd_green/scrapy_jd_green/items.py
@@ -6,7 +6,19 @@
 import scrapy
 
 
-class ScrapyJdGreenItem(scrapy.Item):
+class JobDescription(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
+    url = scrapy.Field()
+    job_title = scrapy.Field()
+    job_role = scrapy.Field()
+    business_details = scrapy.Field()
+    job_description = scrapy.Field()
+    outline = scrapy.Field()
+    job_requirements = scrapy.Field()
+    salary = scrapy.Field()
+    selection_process = scrapy.Field()
+    job_location = scrapy.Field()
+    job_benefits = scrapy.Field()
+    
     pass
