fdc3df0c0154eb6795c51ae2fad94af2f1faca96
Hajime Ozawa <haji.register@gmail.com>
2023-08-07 15:07:33 +0900

    Finish edit parse_item

--- scrapy_jd_green/scrapy_jd_green/spiders/scrapy_jd_green_spider.py
+++ scrapy_jd_green/scrapy_jd_green/spiders/scrapy_jd_green_spider.py
@@ -2,14 +2,13 @@ import scrapy
 from scrapy.http import FormRequest
 from scrapy_jd_green.items import JobDescription
 import json
+from urllib.parse import urljoin
 
 # Read the configuration from config.json
 with open("config.json", "r") as config_file:
     config = json.load(config_file)
     email = config["email"]
     password = config["password"]
-    print(email)
-    print(password)
 
 class ScrapyJdGreenSpiderSpider(scrapy.Spider):
     name = "scrapy_jd_green_spider"
@@ -28,7 +27,7 @@ class ScrapyJdGreenSpiderSpider(scrapy.Spider):
         """
         # Fetch the authenticity token
         authenticity_token = response.css("input[name='authenticity_token']::attr(value)").get()
-        print(authenticity_token)
+
         # Create a form data dictionary with the required information
         formdata = {
             'utf8': '✓',
@@ -50,7 +49,7 @@ class ScrapyJdGreenSpiderSpider(scrapy.Spider):
         if response.status == 200:
             self.logger.info("Successfully logged in.")
             # Start scrawling
-            yield scrapy.Request("https://www.green-japan.com/search_key?key=xmy3i3jpu05ojorlyysq&keyword=AI%E3%82%A8%E3%83%B3%E3%82%B8%E3%83%8B%E3%82%A2", callback=self.parse_results)
+            yield scrapy.Request("https://www.green-japan.com/api/v2/user/job_offers?area_ids%5B%5D=13&company_name_only_flg=false&keyword=AI%E3%82%A8%E3%83%B3%E3%82%B8%E3%83%8B%E3%82%A2&new_flg=false&offset=0&order_type=job_offer_score&salary_bottom_id=&width_size=400", callback=self.parse_results)
 
         else:
             self.logger.error("Login failed.")
@@ -60,34 +59,30 @@ class ScrapyJdGreenSpiderSpider(scrapy.Spider):
         """
         Parse the search results page
         """
-        # Get the job description links
-        for item_link in response.css(".job-card__job-link a::attr(href)").get():
-            yield scrapy.request(response.urljoin(item_link), callback=self.parse_item)
+        # Get the job offer list
+        for offer in json.loads(response.body)["job_offers"]:
+            
+            offer_id = offer["job_offer_id"]
+            client_id = offer["client_id"]
+            url = "/".join(["https://www.green-japan.com/company", str(client_id), "job", str(offer_id)])
+
+            yield scrapy.Request(url, callback=self.parse_item)
 
 
     def parse_item(self, response):
         """
         Parse the job description page
         """
-        url = response.url
-        job_title = response.css('.css-1bp4cqx::text').get()
-        print(job_title)
-        job_role = response.css('.css-1fcpmhi::text')[0].get()
-        print(job_role)
-        business_details = response.css('.css-sr5sh7::text')[0].get()
-        print(business_details)
-        job_description = response.css('.css-sr5sh7::text')[1].get()
-        print(job_description)
-        outline = response.css('.css-abo7h2::text')[1].get()
-        print(outline)
-        job_requirements = response.css('.css-abo7h2::text')[2].get()
-        print(job_requirements)
-        salary = response.css('.css-abo7h2::text')[4].get()
-        print(salary)
-        selection_process = response.css('.css-abo7h2::text')[6].get()
-        print(selection_process)
-        job_location = response.css('.css-abo7h2::text')[7].get()
-        print(job_location)
-        job_benefits = response.css('.css-abo7h2::text')[9].get()
-        print(job_benefits)
 
+        url = response.url
+
+        company_name = response.xpath('//h6[contains(@class, "css-fw3r0w")]/text()').get()
+        job_title = response.xpath('//h1[contains(@class, "css-1bp4cqx")]/text()').get()
+        job_role = response.xpath('//span[contains(@class, "css-9iedg7")]/text()').get()
+        business_details = response.xpath('//h5[contains(@class, "css-sr5sh7") and contains(text(), "事業内容")]/following-sibling::p[contains(@class, "css-bw2zqj")]/text()').get()
+        job_description = response.xpath('//h5[contains(@class, "css-sr5sh7") and contains(text(), "仕事内容")]/following-sibling::p[contains(@class, "css-bw2zqj")]/text()').get()
+        outline = response.xpath('//p[text()="概要"]/following-sibling::p[contains(@class, "css-abo7h2")]/text()').get()
+        job_requirements = response.xpath('//p[text()="概要"]/following-sibling::p[contains(@class, "css-abo7h2")]/text()').get()
+        salary = response.xpath('//span[contains(text(), "万円") and contains(@class, "css-9iedg")]/text()').get()
+        job_location = response.xpath('//p[text()="勤務地"]/following-sibling::p[contains(@class, "css-abo7h2")]/text()').get()
+        job_benefits = response.xpath('//p[text()="待遇・福利厚生"]/following-sibling::p[contains(@class, "css-abo7h2")]/text()').get()
