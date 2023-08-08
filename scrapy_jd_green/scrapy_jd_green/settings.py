b9f2bdf02e9196b8d5493b52bdcc7ca02e9130d6
Hajime Ozawa <haji.register@gmail.com>
2023-08-08 14:35:31 +0900

    Activate pipeline

--- scrapy_jd_green/scrapy_jd_green/settings.py
+++ scrapy_jd_green/scrapy_jd_green/settings.py
@@ -62,9 +62,9 @@ DOWNLOAD_DELAY = 3
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    "scrapy_jd_green.pipelines.ScrapyJdGreenPipeline": 300,
-#}
+ITEM_PIPELINES = {
+    "scrapy_jd_green.pipelines.ScrapyJdGreenPipeline": 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
